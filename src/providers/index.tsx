21d7f1b74fe5a3171a57dc8129950946700a75a1
vlevko <24190241+vlevko@users.noreply.github.com>
2025-04-25 15:03:54 +0300

    practice 5

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,16 +1,43 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useMemo } from "react";
 
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import {
+  ConnectionProvider,
+  WalletProvider as SolanaWalletProvider
+} from "@solana/wallet-adapter-react";
+
 const queryClient = new QueryClient();
 
 export const WalletProvider = ({ children }: PropsWithChildren) => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter()
+    ],
+    []
+  );
+
   return (
-    <WalletModalProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WalletModalProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <SolanaWalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        </WalletModalProvider>
+      </SolanaWalletProvider>    
+    </ConnectionProvider>
   );
 };
